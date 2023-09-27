c126a5b80648c9071349de71fb4396a9d99d3334
dsanzbas <dsanzbas@gmail.com>
2023-09-27 17:34:48 +0200

    Fix issue with tipo_temp variable

--- src/Temperatura_main.java
+++ src/Temperatura_main.java
@@ -2,9 +2,10 @@ import java.io.*;
 public class Temperatura_main {
     public static void main (String[] args) {
         Temperatura temp = new Temperatura();
+        String tipo_temp;
 
         System.out.println("¿Celsius o Farenheit?");
-        String  tipo_temp = System.console().readLine();
+        tipo_temp = System.console().readLine();
         System.out.println("Temperatura de entrada en " + tipo_temp);
         System.out.println("Introduzca la temperatura: ");
         double grados = Double.parseDouble(System.console().readLine());
