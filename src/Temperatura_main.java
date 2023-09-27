d8e99b2591d3cf0d5c0f50334707b7d76fecb3bc
dsanzbas <dsanzbas@gmail.com>
2023-09-27 17:21:48 +0200

    Clase Temperatura_main - v 1.0 Clase Temperatura      - v 1.0

--- src/Temperatura_main.java
+++ src/Temperatura_main.java
@@ -8,12 +8,7 @@ public class Temperatura_main {
         System.out.println("Temperatura de entrada en " + tipo_temp);
         System.out.println("Introduzca la temperatura: ");
         double grados = Double.parseDouble(System.console().readLine());
-
-        if (tipo_temp == "Celsius")
-            temp.celsiusToFarenheit(grados);
-            System.out.println("Temperatura en grados Farenheit: " + temp.celsiusToFarenheit());
-        if (tipo_temp == "Farenheit")
-            temp.farenheitToCelsius(grados);
-            System.out.println("Temperatura en grados Celsius:  " + temp.farenheitToCelsius());
+        System.out.println("Temperatura en grados Farenheit: " + temp.celsiusToFarenheit(grados));
+        System.out.println("Temperatura en grados Celsius:   " + temp.farenheitToCelsius(grados));
     }
 }
