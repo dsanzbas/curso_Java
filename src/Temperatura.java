68db680a7949ca0050396a174ad23c7de9030f0a
dsanzbas <dsanzbas@gmail.com>
2023-09-27 17:26:08 +0200

    Clase Temperatura_main - v 1.0 Clase Temperatura      - v 1.0

--- src/Temperatura.java
+++ src/Temperatura.java
@@ -1,16 +1,12 @@
 public class Temperatura {
-    double grados;
-    double f;
-    double c;
-
     public double celsiusToFarenheit(double grados) {
         final int i = 32;
-        c = (1.8 * grados) + i;
+        double c = (1.8 * grados) + i;
         return c;
     }
     public double farenheitToCelsius(double grados) {
         final double i = 1.8;
-        f = (grados - 32) / i;
+        double f = (grados - 32) / i;
         return f;
     }
 }
