c061d36dba28b66da23864949eeffbbf7f464fcd
dsanzbas <dsanzbas@gmail.com>
2023-10-29 17:39:15 +0100

    Capitulo 5 - Ex. 4 - Estadística (ArrayList)

---
+++ src/Capitulo_5/Ex5_4/Estadistica_AL.java
@@ -0,0 +1,35 @@
+package Capitulo_5.Ex5_4;
+
+import java.util.*;
+
+public class Estadistica_AL {
+    public static void main(String[] args) {
+        int x = 0;
+        Scanner n = new Scanner(System.in);
+
+        System.out.printf("¿Cuántos números va a introducir? ");
+        try {
+            x = n.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Introduzca SOLO números enteros");
+            n.next();
+        }
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        //int[] array = new int[x];
+        int suma = 0;
+        double  media = 0.00;
+
+        for (int i = 0; i < x; i++) {
+            System.out.println("Introduzca el valor " + (i + 1) + " del array");
+            array.add(i,n.nextInt());
+        }
+
+        for (int i = 0; i < x; i++) {
+            suma = suma + array.get(i);
+        }
+
+        media = suma / x;
+
+        System.out.println("La suma de los valores introducidos es " + suma + " y su media es " + media);
+    }
+}
