a5a87d1ba8d209ac0dc6764373fb54d9f9f79571
dsanzbas <dsanzbas@gmail.com>
2023-11-03 17:28:54 +0100

    Capitulo 5 - Ex. 7 - Búsqueda/Ordenación Array Gigante Búsqueda

--- src/Capitulo_5/Ex5_7/Array_Gigante_Busqueda.java
+++ src/Capitulo_5/Ex5_7/Array_Gigante_Busqueda.java
@@ -12,6 +12,13 @@ public class Array_Gigante_Busqueda {
         int input = 0;
         boolean encontrado = false;
         Date date = new Date();
+        Date date1 = new Date();
+        Date date2 = new Date();
+        Date date3 = new Date();
+        Date date4 = new Date();
+        Date date5 = new Date();
+        Date date6 = new Date();
+        Date date7 = new Date();
 
         System.out.println("Introduzca valor a buscar: ");
         try {
@@ -48,18 +55,18 @@ public class Array_Gigante_Busqueda {
             }
         }
 
-        System.out.println("Ordenar Array secuencial - Hora fin    : " + new Timestamp(date.getTime()));
+        System.out.println("Ordenar Array secuencial - Hora fin    : " + new Timestamp(date1.getTime()));
 
         //Ordenar ArrayList
-        System.out.println("Ordenar ArrayList        - Hora inicio : " + new Timestamp(date.getTime()));
+        System.out.println("Ordenar ArrayList        - Hora inicio : " + new Timestamp(date2.getTime()));
 
         Collections.sort(arrayList);
 
-        System.out.println("Ordenar ArrayList        - Hora fin : " + new Timestamp(date.getTime()));
+        System.out.println("Ordenar ArrayList        - Hora fin : " + new Timestamp(date3.getTime()));
 
         // Busqueda array secuencial
 
-        System.out.println("Búsqueda array secuencial - Hora inicio : " + new Timestamp(date.getTime()));
+        System.out.println("Búsqueda array secuencial - Hora inicio : " + new Timestamp(date4.getTime()));
         for (i=0; i < array.length - 1; i++) {
             if (array[i] == input) {
                 encontrado = true;
@@ -67,18 +74,18 @@ public class Array_Gigante_Busqueda {
             }
         }
 
-        System.out.println("Búsqueda array secuencial - Hora fin : " + new Timestamp(date.getTime()));
+        System.out.println("Búsqueda array secuencial - Hora fin : " + new Timestamp(date5.getTime()));
 
         //Búsqueda arrayList
 
-        System.out.println("Búsqueda arrayList        - Hora inicio : " + new Timestamp(date.getTime()));
+        System.out.println("Búsqueda arrayList        - Hora inicio : " + new Timestamp(date6.getTime()));
 
         encontrado = false;
         aux = arrayList.indexOf(input);
 
         if (aux != -1) {encontrado = true;}
 
-        System.out.println("Búsqueda arrayList        - Hora fin    : " + new Timestamp(date.getTime()));
+        System.out.println("Búsqueda arrayList        - Hora fin    : " + new Timestamp(date7.getTime()));
 
         if (encontrado) {
             System.out.println("Array secuencial - Valor encontrado en la posición " + i);
