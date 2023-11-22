5c78b5159eed9c9163a1b9337b53d367abeca813
dsanzbas <dsanzbas@gmail.com>
2023-11-22 07:31:14 +0100

    Capitulo 6 - Ex. 1 - Tabla Multiplicar

--- src/Capitulo_6/Ex6_1/Tabla_Multiplicar.java
+++ src/Capitulo_6/Ex6_1/Tabla_Multiplicar.java
@@ -2,6 +2,7 @@ package Capitulo_6.Ex6_1;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -9,18 +10,33 @@ import java.util.Scanner;
 public class Tabla_Multiplicar {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean dir_created = false;
+        boolean file_created = false;
         try {
             System.out.println("Introducir un número entre el 1 y el 10: ");
-            int i = input.nextInt();
+            int intg = input.nextInt();
 
-            File file = new File("C:\\Users\\crist\\Desktop\\Ex6_1", "Ex6_1.txt");
+            File file = new File("C:\\Users\\dsanzbas\\Desktop\\Ex6_1\\fichero.txt");
+            File txt  = file.getParentFile();
 
-            boolean dir_created = file.mkdir();
-            boolean file_created = file.createNewFile();
+            if (!txt.exists()) {
+                dir_created = txt.mkdir();}
 
-            if ((dir_created) && (file_created)) {
-                FileOutputStream output = new FileOutputStream(file);
+            if (file.exists()) {
+                file_created = file.createNewFile();
+            } //else {
+            //    file.delete();
+            //    file.createNewFile();
+            //}
+
+            String cadena_output;
+            FileWriter output = new FileWriter(file);
+            for (int i = 1; i <= 10; i++) {
+                cadena_output = i + " * " + intg + " = " + (i * intg);
+                output.write(cadena_output);
             }
+            output.close();
+
         } catch (InputMismatchException e) {
             System.out.println("Formato numérico incorrecto");
             input.nextLine();
