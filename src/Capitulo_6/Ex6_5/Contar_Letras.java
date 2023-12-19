72d8dc44c2a977ee11f62f875dd34095c668c774
dsanzbas <dsanzbas@gmail.com>
2023-12-19 07:29:10 +0100

    Capitulo 6 - Ex6_5 - Contar_Letras.java > revisar y corregir errores sugeridos por SonarLint

--- src/Capitulo_6/Ex6_5/Contar_Letras.java
+++ src/Capitulo_6/Ex6_5/Contar_Letras.java
@@ -1,22 +1,19 @@
 package Capitulo_6.Ex6_5;
 
 import java.io.*;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contar_Letras {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        Contar_Letras tm = new Contar_Letras();
-        String package_name = tm.getClass().getPackageName();
-
+        System.out.println("Introducir nombre de fichero: ");
+        String fichero = scanner.nextLine();
+        FileReader input = null;
+        BufferedReader bf = null;
         try {
-            System.out.println("Introducir nombre de fichero: ");
-            String fichero = scanner.nextLine();
-            //String dir = System.getProperty("user.dir") + "\\" + package_name + "\\fichero1.txt";
             String dir = System.getProperty("user.dir") + "\\" + fichero + ".txt";
             File file = new File(dir);
-            File txt  = file.getParentFile();
+            File txt = file.getParentFile();
 
             if (!txt.exists()) {
                 System.out.println("El directorio no existe");
@@ -27,24 +24,26 @@ public class Contar_Letras {
                 System.exit(1);
             }
 
-            String cadena_input;
+            String cadenaInput;
             int i = 0;
             Contador bc = new Contador();
-            FileReader input = new FileReader(file);
-            BufferedReader bf = new BufferedReader(input);
-            while ((cadena_input = bf.readLine()) != null)
-            {
-                for(i=0; i < cadena_input.length(); i++)
-                {
-                    bc.buscarCaracter(cadena_input.charAt(i));
+            input = new FileReader(file);
+            bf = new BufferedReader(input);
+            while ((cadenaInput = bf.readLine()) != null) {
+                for (i = 0; i < cadenaInput.length(); i++) {
+                    bc.buscarCaracter(cadenaInput.charAt(i));
                 }
             }
             int[] total = bc.getTotal();
-            char[] abc  = bc.getAbc();
-            for (i=0; i < abc.length; i++) {
+            char[] abc = bc.getAbc();
+            for (i = 0; i < abc.length; i++) {
                 System.out.println("Nº veces letra " + abc[i] + " :" + total[i]);
             }
 
         } catch (IOException e) {e.printStackTrace();}
+        finally {
+            if (input != null) {input.close();}
+            if (bf    != null) {bf.close();}
+        }
     }
 }
